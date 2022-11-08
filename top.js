3e01047ff1de2fadf2a10c1fa063b8df52049722
xcanhide <115264136+xcanhide@users.noreply.github.com>
2022-11-08 19:15:26 +0200

    Add files via upload

--- top.js
+++ top.js
@@ -3,16 +3,24 @@ menubtn = document.getElementById("mnubtn");
 closebtn = document.getElementById("closebtn");
 window.onscroll = function() {
   scrollFunction()
+  menuFunction()
 };
 
 function scrollFunction() {
   var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
-  if (currentScroll > 0) {
+  if (currentScroll > 100) {
     topbtn.style.transform = "translateY(0rem)";
+  } else {
+    topbtn.style.transform = "translateY(3rem)";
+  }
+}
+
+function menuFunction() {
+  var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+  if (currentScroll > 0) {
     menubtn.style.transform = "translateX(0rem)";
     closebtn.style.transform = "translateX(0rem)";
   } else {
-    topbtn.style.transform = "translateY(3rem)";
     menubtn.style.transform = "translateX(-2.15rem)";
     closebtn.style.transform = "translateX(-2.15rem)";
   }
